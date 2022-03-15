816d801fc11d49fad989094ec8a06ef0b7ba54d0
Graylands <graylandsgreen@gmail.com>
2022-03-15 16:30:06 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
-
 /**
  * main - prints _putchar
+ * Description: print _putchar
+ * Return: 0 (Success)
  *
- * Description: program prints _putchar.
- *
- * Return: 0 on success.
  */
-
 int main(void)
+
 {
-	char text[9] = "_putchar";
-	int i;
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
