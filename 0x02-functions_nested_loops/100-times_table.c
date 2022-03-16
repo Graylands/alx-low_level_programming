401598e0ec6dae29d4f6f48c005670f847a8931a
Graylands <graylandsgreen@gmail.com>
2022-03-16 04:31:59 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,17 +5,18 @@
  * @n: number of the times table
  */
 void print_times_table(int n)
-}
 int i, j, k;
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i <= n; i)
+{
+for (j = 0; j <= n; j++)
 {
 k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} 
+}
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -23,7 +24,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+}
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
@@ -41,7 +42,6 @@ _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-_putchar('\n');																
-}
+_putchar('\n');
 }
 }
