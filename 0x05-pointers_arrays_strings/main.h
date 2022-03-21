bcd5635c95f5e05acec3a68e4a97f32f1e4e8bc7
Graylands <graylandsgreen@gmail.com>
2022-03-21 08:15:09 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,19 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#include <stdio.h>
 
-void reset_to_98(int *n);#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+void reset_to_98(int *);
 
-void reset_to_98(int *n);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
+}
