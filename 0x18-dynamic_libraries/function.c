6f511dced14bebf8eb813772ee88ae974f66594f
Graylands <graylandsgreen@gmail.com>
2022-06-13 18:07:29 -0700

    commit

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* op_add - calculates the sum of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+       return (a + b);
+}
+
+/**
+* op_sub - calculates the difference of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: difference of a and b
+*/
+int op_sub(int a, int b)
+{
+       return (a - b);
+}
+
+/**
+* op_mul - calculates the product of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: product of a and b
+*/
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+* op_div - calculates the division of two integers
+* @a: first integer
+* @b: second integer
+* Return: remainder of division of the numbers
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
